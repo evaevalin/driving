d9425ca041c7e3865de1933b194ad27e784fccf2
luli <evalin19920321@gmail.com>
2019-04-12 18:44:20 +0800

    add us part

--- driving.py
+++ driving.py
@@ -6,5 +6,8 @@ if country == '台灣':
        print('你可以考駕照')
    else:
    	   print('你不能考駕照')
-else:
-	print('我不知道優')
+elif country =='美國':
+    if age >= 16:
+	    print('你可以考駕照')
+    else:
+    	print('你不可以考駕照')
